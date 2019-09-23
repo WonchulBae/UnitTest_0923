10b3e691487bb9490f0f627063a013058a2ba4ab
Chansik Yun <chansigi@gmail.com>
2019-09-23 15:31:43 +0900

    .

---
+++ src/com/lge/ex5/TerminalTest.java
@@ -0,0 +1,25 @@
+package com.lge.ex5;
+
+import java.io.IOException;
+
+public class TerminalTest {
+}
+
+// SUT
+class Terminal {
+    public void connect() throws Exception {
+    }
+
+    public void disconnect() throws Exception {
+    }
+
+    public void login(String id, String password) {
+    }
+
+    public void logout() {
+    }
+
+    public boolean isLogin() {
+        return true;
+    }
+}
