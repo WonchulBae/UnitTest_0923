87c0adc395241b9116aaac59df4c9851c662c61d
Chansik Yun <chansigi@gmail.com>
2019-09-24 13:31:36 +0900

    .

--- src/com/lge/ex12/MockitoSample.java
+++ src/com/lge/ex12/MockitoSample.java
@@ -8,7 +8,20 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
+class Point {
+    private int x;
+    private int y;
+
+    public void move(int x, int y) {}
+}
+
 class SUT {
+    public void hoo(Point point) {
+        point.move(10, 20);
+        point.move(10, 30);
+        point.move(10, 40);
+    }
+
     public void foo(List<String> s) {
         s.add("one");
         s.add("two");
@@ -16,16 +29,37 @@ class SUT {
 
     public void goo(List<String> s) {
         s.add("once");
+        s.add("once");
         s.add("twice");
         s.add("twice");
     }
 }
 
 public class MockitoSample {
+    @Test
+    public void hooTest() throws Exception {
+        Point mockedPoint = mock(Point.class);
+        SUT sut = new SUT();
 
+        sut.hoo(mockedPoint);
+
+        // verify(mockedPoint).move(10, 20);
+        // verify(mockedPoint).move(10, 30);
+        // verify(mockedPoint).move(10, 40);
+
+        verify(mockedPoint).move(eq(10), anyInt());
+    }
+    
+
+    // - 호출 횟수 지정 방법
     // times(1): 정확한 횟수를 지정한다.
-    // atLeast(N), atLeastOnce: N번 이상
-    // atMost(N), atMostOnce: N번 이하
+    // atLeast(N), atLeastOnce(): N번 이상
+    // atMost(N), atMostOnce(): N번 이하
+
+    // - 호출 인자
+    //   anyXXX()
+    //  : 인자는 상관하지 않고, 함수의 호출 여부만을 검증할 수 있습니다.
+
     @Test
     public void gooTest() throws Exception {
         List<String> mockedList = mock(List.class);
