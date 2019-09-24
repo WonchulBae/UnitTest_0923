00098fe15be550bc1a9824b0d8f8d6924e88b0b2
Chansik Yun <chansigi@gmail.com>
2019-09-24 10:29:30 +0900

    .

--- src/com/lge/ex9/UserManagerTest.java
+++ src/com/lge/ex9/UserManagerTest.java
@@ -1,7 +1,44 @@
 package com.lge.ex9;
 
-public class UserManagerTest {
+import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+//  => Fake Object Patterns
+// 1) 아직 만들어지지 않은 컴포넌트에 의존하는 객체를 검증하고 싶다.
+// 2) 의존 객체가 너무 느려서, 테스트하기 힘들다.
+// 3) 의존 객체를 사용하기 어려울 때 사용할 수 있다.
+
+class FakeDatabase implements Database {
+    private Map<String, User> data = new HashMap<>();
+
+    @Override
+    public void save(String name, User user) {
+        data.put(name, user);
+    }
+
+    @Override
+    public User load(String name) {
+        return data.get(name);
+    }
+}
+
+
+public class UserManagerTest {
+    @Test
+    public void createTest() throws Exception {
+        String name = "Tom";
+        int age = 42;
+        UserManager manager = new UserManager(new FakeDatabase());
+        User expected = new User(name, age);
+
+        manager.create(name, age);
+
+        assertEquals(expected, manager.find(name));
+    }
 }
 
 // SUT
@@ -17,11 +54,13 @@ class User {
 
 interface Database {
     void save(String name, User user);
+
     User load(String name);
 }
 
 class UserManager {
     private Database database;
+
     public UserManager(Database database) {
         this.database = database;
     }
